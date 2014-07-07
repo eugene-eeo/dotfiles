16eb9730de56b5ab9e7fc156e3e25b23f5b3b6ee
Eugene Eeo <xe0jun@hotmail.com>
2014-07-07 21:58:01 +0800

    add alias for du and json

--- bash-files/.bash-files/aliases.sh
+++ bash-files/.bash-files/aliases.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 alias tree='tree -F -C -I "*.pyc|__pycache__|*.pyo"'
+alias json='python -m json.tool'
 alias tk='toolkit'
 alias ls='ls -G'
+alias du='du -h'
 
 cd() {
     pushd $@ > /dev/null
