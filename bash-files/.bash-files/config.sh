a498640c499d662406d89c36a412888707177093
Eugene Eeo <xe0jun@hotmail.com>
2014-10-05 09:29:55 +0800

    remove unused files

--- bash-files/.bash-files/config.sh
+++
@@ -1,9 +0,0 @@
-if [ `id -u` == '0' ]; then
-    echo -e '\033[31mYou are in sudo mode!\033[0m'
-fi
-
-#export PS1=' 🌀  '
-#export PS1=" \[\e[38;5;18m\]>\[\e[38;5;19m\]>\[\e[38;5;20m\]>\[\e[0m\] "
-#export PS1=" \[\e[38;5;238m\][\[\e[38;5;243m\]\w\[\e[38;5;238m\]] \[\e[38;5;24m\]$\[\e[0m\] "
-export PS1=" \[\e[38;5;24m\]$\[\e[0m\] "
-export PS2=" \[\e[38;5;239m\]>\[\e[0m\] "
