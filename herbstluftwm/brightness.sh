548d68879d2189d21627396700d4fb31eba8f15e
Eeo Jun <141bytes@gmail.com>
2018-11-23 19:39:23 +0000

    proper brightness support

--- herbstluftwm/brightness.sh
+++ herbstluftwm/brightness.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-CURRNAME=$(xrandr | grep " connected" | cut -f1 -d " ")
-CURRBRIGHT=$(xrandr --current --verbose | grep -m 1 'Brightness:' | cut -f2- -d:)
-if [ "$1" = "+" ] && [ $(echo "$CURRBRIGHT < 1.25" | bc) -eq 1 ]
+MIN='100'
+MAX=$(pkexec xfpm-power-backlight-helper --get-max-brightness)
+CUR=$(pkexec xfpm-power-backlight-helper --get-brightness)
+
+if [ "$1" = "+" ] && [ $(echo "$CUR < $MAX" | bc) -eq 1 ]
 then
-    xrandr --output $CURRNAME --brightness $(echo "$CURRBRIGHT * 1.05" | bc)
-elif [ "$1" = "-" ] && [ $(echo "$CURRBRIGHT > 0" | bc) -eq 1 ]
+    pkexec xfpm-power-backlight-helper --set-brightness $(echo "($CUR*1.25)/1" | bc)
+elif [ "$1" = "-" ] && [ $(echo "$CUR > $MIN" | bc) -eq 1 ]
 then
-    xrandr --output $CURRNAME --brightness $(echo "$CURRBRIGHT * 0.95" | bc)
+    pkexec xfpm-power-backlight-helper --set-brightness $(echo "($CUR*0.75)/1" | bc)
 fi
