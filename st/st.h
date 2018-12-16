968c3fe4aabaefcc2600b90cef2e2c95a0088b1b
Eeo Jun <141bytes@gmail.com>
2018-12-16 14:13:18 +0000

    remove iso14755 feature

--- st/st.h
+++ st/st.h
@@ -85,7 +85,6 @@ void die(const char *, ...);
 void redraw(void);
 void draw(void);
 
-void iso14755(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
