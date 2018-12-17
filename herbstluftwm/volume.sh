1bc13299fbfebed2526b85ba6308b30e2661c0f1
Eeo Jun <141bytes@gmail.com>
2018-12-17 13:59:22 +0000

    update volume script

--- herbstluftwm/volume.sh
+++ herbstluftwm/volume.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
-vol() {
-    amixer get Master | tail -n 1 | grep -o '[0-9]\+%' | grep -o '[0-9]\+'
-}
-
-volume=`vol`
-
-if [ "$1" = "+" ] && [ $(echo "$volume < 100" | bc) -eq 1 ]
-then
-    pactl set-sink-mute @DEFAULT_SINK@ 0
-    pactl set-sink-volume @DEFAULT_SINK@ '+2%'
-elif [ "$1" = "-" ] && [ $(echo "$volume >= 0" | bc) -eq 1 ]
-then
-    pactl set-sink-volume @DEFAULT_SINK@ '-2%'
-fi
+pactl set-sink-mute @DEFAULT_SINK@ 0
+case "$1" in
+    +) pactl set-sink-volume @DEFAULT_SINK@ '+2%' ;;
+    -) pactl set-sink-volume @DEFAULT_SINK@ '-2%' ;;
+esac
