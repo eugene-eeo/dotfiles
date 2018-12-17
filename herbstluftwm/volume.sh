c1c3a389b4c8e7cadb9a2cfe795b6bb7d69d653d
Eeo Jun <141bytes@gmail.com>
2018-12-17 17:16:29 +0000

    update

--- herbstluftwm/volume.sh
+++ herbstluftwm/volume.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sink='@DEFAULT_SINK@'
-pactl set-sink-mute "$sink" 0
 case "$1" in
-    +) pactl set-sink-volume "$sink" '+2%' ;;
-    -) pactl set-sink-volume "$sink" '-2%' ;;
+    +) pactl set-sink-mute "$sink" 0; pactl set-sink-volume "$sink" '+2%' ;;
+    -) pactl set-sink-mute "$sink" 0; pactl set-sink-volume "$sink" '-2%' ;;
+    m) pactl set-sink-mute "$sink" toggle ;;
 esac
