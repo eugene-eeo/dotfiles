7145355640f3713bc60259a5a58ad1af006f000c
Eeo Jun <141bytes@gmail.com>
2018-12-20 00:53:10 +0000

    fix emoji crashing

--- st/config.h
+++ st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Consolas:pixelsize=18:antialias=true";
-//static char *font = "Source Code Pro:medium:pixelsize=18:antialias=true";
+static char *font = "Consolas:pixelsize=18:antialias=true:autohint=true";
+//static char *font = "Source Code Pro:medium:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
