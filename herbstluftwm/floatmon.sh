195cb2c35327f6ec3b7dba4ca9305390395e7843
Eeo Jun <141bytes@gmail.com>
2019-04-27 13:52:47 +0100

    proper floating layer support

--- herbstluftwm/floatmon.sh
+++ herbstluftwm/floatmon.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-monitor='floatmon'
 tag='#'
 Mod='Super'
 
@@ -21,29 +20,29 @@ size=$(xwininfo -root |
                   }')
 
 hc chain , add "$tag" , floating "$tag" on
-hc or , add_monitor "${size}+0+0" "$tag" "$monitor" \
-      , move_monitor "$monitor" "${size}+0+0"
-hc raise_monitor "$monitor"
-hc lock_tag "$monitor"
+hc or , add_monitor "${size}+0+0" "$tag" floatmon \
+      , move_monitor floatmon "${size}+0+0"
+hc pad floatmon 20
+hc lock_tag floatmon
 
 cmd=(
 or  case: and
         # if not on the floating monitor, and a client is focused
         # then remember last monitor of client, and move client to
         # floating tag
-        . compare monitors.focus.name != "$monitor"
+        . compare monitors.focus.name != floatmon
         . get_attr clients.focus.winid
         . chain try new_attr string clients.focus.my_lastmon
                 try true
         . substitute M monitors.focus.index
             set_attr clients.focus.my_lastmon M
-        . shift_to_monitor "$monitor"
-        . focus_monitor "$monitor"
+        . shift_to_monitor floatmon
+        . focus_monitor floatmon
         . true
     case: and
         # if on floating monitor, and client is focused then send
         # it back to where it came from
-        . compare monitors.focus.name != "$monitor"
+        . compare monitors.focus.name = floatmon
         . get_attr clients.focus.winid
         . substitute M clients.focus.my_lastmon chain
             , shift_to_monitor M
