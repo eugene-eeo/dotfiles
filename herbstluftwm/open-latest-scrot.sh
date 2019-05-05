c524ec33dbbf90cf80c0a10152d8f7f476fcea36
Eeo Jun <141bytes@gmail.com>
2019-05-05 19:17:13 +0100

    open-latest-scrot script + binding

---
+++ herbstluftwm/open-latest-scrot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+latest() {
+    # shellcheck disable=2010
+    ls ~/Pictures/ \
+        | grep -E '^[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]+_.+\.png' \
+        | sort -r \
+        | head -n 1
+}
+
+xdg-open "$HOME/Pictures/$( latest )"
