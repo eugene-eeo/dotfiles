d77334fb5c8afdc3708c465b49b440b583a353bd
Eeo Jun <141bytes@gmail.com>
2019-05-15 21:45:24 +0100

    up with the times

--- herbstluftwm/open-latest-scrot.sh
+++ herbstluftwm/open-latest-scrot.sh
@@ -2,7 +2,7 @@
 latest() {
     # shellcheck disable=2010
     ls ~/Pictures/ \
-        | grep -E '^[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]+_.+\.png' \
+        | grep -E '^[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]+\.png' \
         | sort -r \
         | head -n 1
 }
