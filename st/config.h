4c4ce81a4ade33b7a51f21f5220263365b2e8f3f
Eeo Jun <141bytes@gmail.com>
2019-05-17 00:33:09 +0100

    increase border

--- st/config.h
+++ st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Consolas:pixelsize=18:antialias=true:autohint=true";
-static int borderpx = 2;
+static int borderpx = 4;
 
 /*
  * What program is execed by st depends of these precedence rules:
