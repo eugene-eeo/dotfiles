0f3392ba873b1a38f09db3b4610bf8ba674c5a20
Eeo Jun <141bytes@gmail.com>
2019-09-08 21:54:28 +0800

    update

--- herbstluftwm/calendar.sh
+++ herbstluftwm/calendar.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
 # pop-up calendar for dzen
-# shellcheck disable=2004
 
 width=220
 monitor=( $(herbstclient list_monitors \
@@ -9,11 +8,11 @@ monitor=( $(herbstclient list_monitors \
     | cut -d' ' -f2 \
     | tr x ' ' \
     | sed 's/\([-+]\)/ \1/g') )
-x=$((${monitor[2]} + ${monitor[0]}/2 - width/2))
+x=$((monitor[2] + monitor[0]/2 - width/2))
 padding=( $(herbstclient list_padding) )
 
 
-today=$(( $(date +'%d') + 0))
+today=$(( "10#$(date +'%d')" + 0))
 
 {
     echo
@@ -23,7 +22,7 @@ today=$(( $(date +'%d') + 0))
               -e 's/ Su Mo Tu We Th Fr Sa /^bg(#333333)\0^bg()/' \
               -e "s/(^| )( ${today} )($| )/\\1^bg(#900603)^fg(#FFFFFF)\\2^fg()^bg()\\3/"
 } \
-    | dzen2 -p 60 -x $x -y "$(( ${padding[0]} * 2 ))" -w $width -l 8 -sa c \
+    | dzen2 -p 60 -x $x -y "$(( padding[0] * 2 ))" -w $width -l 8 -sa c \
             -e 'onstart=uncollapse;button1=exit' \
-            -fg '#EEEEEE' -bg '#000000' \
-            -fn 'Consolas:weight=bold:pixelsize=16'
+            -fg '#EEEEEE' -bg '#111111' \
+            -fn 'Iosevka Term:weight=bold:pixelsize=18'
