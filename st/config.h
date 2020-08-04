d53763dc9825b854834739493aa59955c88b95de
Eeo Jun <141bytes@gmail.com>
2020-08-04 23:32:29 +0100

    use fontconfig size=12

--- st/config.h
+++ st/config.h
@@ -8,7 +8,7 @@
 /* static char *font = "Iosevka Term:pixelsize=18:autohint=true:antialias=true"; */
 /* static char *font = "Consolas:pixelsize=18:autohint=true:antialias=true"; */
 static char *font = "Consolas:size=12:autohint=true:antialias=true";
-/* static char *font = "Cascadia Mono PL:pixelsize=17:autohint=true:antialias=true"; */
+/* static char *font = "Cascadia Mono PL:size=12:autohint=true:antialias=true"; */
 static int borderpx = 2;
 
 /*
