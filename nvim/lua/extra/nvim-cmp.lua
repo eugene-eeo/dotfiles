359bd578b304b8a4d09aafba141b352a5c290476
Eeo Jun <141bytes@gmail.com>
2023-03-15 23:13:56 +0000

    I miss S-Tab

--- nvim/lua/extra/nvim-cmp.lua
+++ nvim/lua/extra/nvim-cmp.lua
@@ -30,7 +30,9 @@ cmp.setup {
             end
         end, { 'i', 's' }),
         ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if luasnip.locally_jumpable(-1) then
+            if cmp.visible() then
+                cmp.select_prev_item(select_behavior)
+            elseif luasnip.locally_jumpable(-1) then
                 luasnip.jump(-1)
             else
                 fallback()
