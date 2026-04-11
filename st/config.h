4768201d4bf3d45a0742fea9dfc4cb6822546328
Eeo Jun <141bytes@gmail.com>
2026-04-11 21:03:49 +0100

    st: bump to 0.9.3 with fullscreen patch

--- st/config.h
+++ st/config.h
@@ -10,9 +10,6 @@ static char *font = "Iosevka Term:weight=semibold:size=12:autohint=true:antialia
 /* static char *font = "Ubuntu Mono:size=13:autohint=true:antialias=true"; */
 /* static char *font = "Menlo:size=11:autohint=true:antialias=true"; */
 /* static char *font = "Hack:pixelsize=15:autohint=true:antialias=true"; */
-/* static char *font = "Fira Mono:size=12:weight=medium:autohint=true:antialias=true"; */
-/* static char *font = "SF Mono:weight=semibold:size=12:autohint=true:antialias=true"; */
-/* static char *font = "Cascadia Mono PL:size=12:autohint=true:antialias=true"; */
 static int borderpx = 2;
 
 /*
